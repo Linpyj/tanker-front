0cae536d60b19b1026e01eda1bfa5d8ec59ad2c6
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-22 00:27:14 +0900

    move login comp

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/components/About.vue'
-import Login from '@/components/Login.vue'
+import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
