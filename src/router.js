c4cf1d7afab417a7902ab0b389735c2cb31895d5
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-21 23:53:56 +0900

    create login page

--- src/router.js
+++ src/router.js
@@ -3,18 +3,24 @@ import Router from 'vue-router'
 
 import Home from '@/components/Home.vue'
 import About from '@/components/About.vue'
+import Login from '@/components/Login.vue'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
     { 
-      path: '/',
-      component: Home
+        path: '/',
+        component: Home
     },
     {
-      path: '/about',
-      component: About
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/login',
+        component: Login
     }
   ]
 })
