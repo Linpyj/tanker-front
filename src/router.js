283f810d216f7bfbebe8d5f26e2a41502862d659
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-22 10:24:54 +0900

    /にアクセスした時にloginページが表示されるようにした

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
 import About from '@/components/About.vue'
 import Login from '@/views/Login.vue'
 import Signup from '@/views/Signup.vue'
@@ -12,15 +11,11 @@ export default new Router({
   routes: [
     { 
         path: '/',
-        component: Home
+        component: Login
     },
     {
         path: '/about',
         component: About
-    },
-    {
-        path: '/login',
-        component: Login
     },
     {
         path: '/signup',
