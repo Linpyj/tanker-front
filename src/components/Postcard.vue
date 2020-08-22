bc8f73db6debfc6389f41fc0fd945f290c073c36
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-22 23:26:55 +0900

    fix

--- src/components/Postcard.vue
+++ src/components/Postcard.vue
@@ -5,8 +5,11 @@
                 <v-card>
                     <v-card-title></v-card-title>
                     <v-card-text class="headline font-weight-bold">
-                        {{ item }}
+                        {{ item.content }}
                     </v-card-text>
+                    <div class="has-text-centered">
+                        <p>{{ item.created_at }}</p>
+                    </div>
                 </v-card>
             </v-col>
         </v-row>
