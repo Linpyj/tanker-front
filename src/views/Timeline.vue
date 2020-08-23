1a7e5cfb4cf83c36271847ce47ddd968312744ed
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-23 10:44:03 +0900

    add ダミー data

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -21,7 +21,11 @@ export default {
                 {"id": 1, "user_id": 7, "content": '山彦や　やまびこやまびこ　山彦や', "created_at": "2020-08-03"},
                 {"id": 2, "user_id": 4, "content": '古池や　買わず買わず買わず　古池や', "created_at": "2020-08-03"},
                 {"id": 3, "user_id": 3, "content": '春過ぎて夏来にけらし白妙の衣干すてふ天の香具山', "created_at": "2020-08-04"},
-                {"id": 4, "user_id": 6, "content": '秋の田のかりほの庵の苫をあらみわが衣手は露にぬれつつ', "created_at": "2020-08-05"}
+                {"id": 4, "user_id": 6, "content": '秋の田のかりほの庵の苫をあらみわが衣手は露にぬれつつ', "created_at": "2020-08-05"},
+                {"id": 5, "user_id": 17, "content": '山彦や　やまびこ　山彦や', "created_at": "2020-08-03"},
+                {"id": 6, "user_id": 2, "content": '古池や　買わず買わず　古池や', "created_at": "2020-08-03"},
+                {"id": 7, "user_id": 1, "content": '春過ぎて衣干すてふ天の香具山', "created_at": "2020-08-04"},
+                {"id": 8, "user_id": 5, "content": '秋の田のらみわが衣手は露にぬれつつ', "created_at": "2020-08-05"}
             ]
         }
     },
