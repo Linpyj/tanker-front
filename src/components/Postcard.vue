398623016107cc67292c7253ad514aeb7db41ecd
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-08-23 10:32:50 +0900

    ホットリロード設定追加

--- src/components/Postcard.vue
+++ src/components/Postcard.vue
@@ -4,12 +4,15 @@
       <v-col cols="7">
         <v-card
           outlined
+          height="100%"
         >
           <v-card-text class="headline">
             {{ item.content }}
           </v-card-text>
           
-          <v-card-actions>
+          <v-card-actions
+            height="10%"
+            class="mt-5">
             <v-btn text icon color="lighten-5">
               <v-icon>mdi-heart</v-icon>
             </v-btn>
