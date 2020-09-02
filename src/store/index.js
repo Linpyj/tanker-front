6db2d6be310c5fb8c737116ffd44d403054b7de7
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-09-02 11:14:36 +0900

    install vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-// import createPersistedState from 'vuex-persistedstate';
+import createPersistedState from 'vuex-persistedstate';
 import user from './modules/user'
 import post from './modules/post'
 
@@ -10,7 +10,12 @@ const store = new Vuex.Store({
 	modules: {
 		user,
 		post
-	}
+	},
+	plugins: [createPersistedState({
+        key: 'tanker',
+        // paths: ['user.token'],
+        storage: window.sessionStorage
+    })]
 	// plugins: [createPersistedState({
 	// 	key: "tanker",
 	// 	paths: ["auth.state.token"],
