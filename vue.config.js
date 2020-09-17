b7881e584663925a958f3afa49f82f25727879af
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-09-17 16:42:32 +0900

    vue.config.js内の不要な記述を削除

--- vue.config.js
+++ vue.config.js
@@ -14,13 +14,7 @@ module.exports = {
       // 9/17
       proxy: {
         "/api/": {
-          // 正しいコード
           target: process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://guarded-wildwood-22546.herokuapp.com"
-          
-          // target: 'http://localhost:3000'
-
-          // ダミー
-          // target: process.env.NODE_ENV === 'development' ? "http://localhost:30000" : "https://guarded-wildwood-22546.herokuapp.com"
         }
       },
       host: 'localhost',
