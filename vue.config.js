73f7bfe01259a980bcac1d633b56ba60fa58054f
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-09-17 18:36:51 +0900

    fix

--- vue.config.js
+++ vue.config.js
@@ -12,12 +12,19 @@ module.exports = {
       // },
       //　追記ここまで
       // 9/17
+      // axios: {
+      //   baseURL: process.env.BASE_URL	|| '/'	// 追記
+      // },
       axios: {
-        baseURL: process.env.BASE_URL	|| '/'	// 追記
+        // axios でproxy が使えるようにする
+        proxy: true
       },
       proxy: {
+        // "/api/": {
+        //   target: process.env.NODE_ENV === 'production' ? "https://guarded-wildwood-22546.herokuapp.com" : "http://localhost:3000"
+        // }
         "/api/": {
-          target: process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://guarded-wildwood-22546.herokuapp.com"
+          target: "https://guarded-wildwood-22546.herokuapp.com"
         }
       },
       // host: 'localhost',
