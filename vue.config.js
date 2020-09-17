b0f4bfaacc66f3e2ac44966b0b54be1101b0e7a2
藤井麟太郎 <atarimaetaisou@icloud.com>
2020-09-17 18:38:08 +0900

    fix

--- vue.config.js
+++ vue.config.js
@@ -15,10 +15,10 @@ module.exports = {
       // axios: {
       //   baseURL: process.env.BASE_URL	|| '/'	// 追記
       // },
-      axios: {
-        // axios でproxy が使えるようにする
-        proxy: true
-      },
+      // axios: {
+      //   // axios でproxy が使えるようにする
+      //   proxy: true
+      // },
       proxy: {
         // "/api/": {
         //   target: process.env.NODE_ENV === 'production' ? "https://guarded-wildwood-22546.herokuapp.com" : "http://localhost:3000"
